aee8eca819eb38dd81be1d7b40844fd44db61daa
Gerardo Velez <gvelez@fender.com>
2020-10-06 14:46:47 -0700

    Sig up Page: User Roles are filled up dynamically

--- src/main/java/edu/cetys/springlabs/controller/SignUpController.java
+++ src/main/java/edu/cetys/springlabs/controller/SignUpController.java
@@ -1,8 +1,13 @@
 package edu.cetys.springlabs.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import edu.cetys.springlabs.enums.UserRole;
 
 
 @Controller
@@ -11,6 +16,13 @@ public class SignUpController {
 	@GetMapping("/sign-up")
 	public String signUp(Model model) {
 	
+		List<String> userRoles = new ArrayList<String>();
+		
+		userRoles.add(UserRole.TOURIST.toString());
+		userRoles.add(UserRole.VINTNER.toString());
+		
+		model.addAttribute("userRoles", userRoles);
+		
 		return "sign-up";
 	}
 	
